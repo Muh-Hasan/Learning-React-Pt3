d841eabc8045fd9bf5192d27c428f3011e5f51ee
Muhammad Hasan <muh.hasan.git@gmail.com>
2020-06-09 15:18:15 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function New(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="text">
+    <h3>My Name is {props.name}</h3>
+    <h3>My Age is {props.age}</h3>
+  </div>
+
   );
 }
 
-export default App;
+
+export default New;
